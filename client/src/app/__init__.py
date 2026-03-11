72b2f766a5e8f9a1b804f67de3b70758d9a74171
MAX1T1A <max1t1aldan@gmail.com>
2026-03-11 17:25:38 +0300

    fix bugs sockets

--- client/src/app/__init__.py
+++ client/src/app/__init__.py
@@ -3,12 +3,13 @@ import asyncio
 from app.renderer import FeedRenderer
 from app.transport import Transport
 from app.transport.http import HTTPTransport
+from app.transport.ws import WSTransport
 from app.watcher import SystemWatcher
 
 
 async def main() -> None:
-    transport: Transport = HTTPTransport(
-        server_url="https://max1t1a-statusphere-znycvf-8e6cc4-193-181-208-72.traefik.me",
+    transport: Transport = WSTransport(
+        server_url="https://sphere.ug3n.com",
         token="my-room-token",
     )
     watcher = SystemWatcher(on_change=transport.send)
