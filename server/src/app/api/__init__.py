00e8e5e8559f4382ae50f68f283dcb84e5addefa
MAX1T1A <max1t1aldan@gmail.com>
2026-03-11 17:46:38 +0300

    fix bugs

--- server/src/app/api/__init__.py
+++ server/src/app/api/__init__.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 
-from .routes.feed import router as feed_router
-from .routes.status import router as status_router
 from .routes.ws import router as ws_router
 
 
 def register_routers(app: FastAPI) -> None:
-    app.include_router(status_router)
-    app.include_router(feed_router)
     app.include_router(ws_router)
